b525e50d2165f1ca79ddcea30aa766faee09517d
Dustin Specker <DustinSpecker@DustinSpecker.com>
2014-09-21 14:07:30 -0500

    Adding routes

--- server.js
+++ server.js
@@ -3,6 +3,22 @@
 var hapi = require('hapi')
   , server = new hapi.Server(3000);
 
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello, world!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: function (request, reply) {
+    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+  }
+});
+
 server.start(function () {
   console.log('Server running at:', server.info.uri);
 });
